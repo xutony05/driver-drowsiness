1a0ea6cd073a3eec92637cc678e76009ec649677
Tony Xu <33339063+xutony05@users.noreply.github.com>
2023-09-22 13:39:32 -0400

    modify batch script

--- model/Transformer/HPC/ccan_script.sh
+++ model/Transformer/HPC/ccan_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH -A def-snikan
-#SBATCH --time 0-00:40:00
+#SBATCH --time 0-00:25:00
 #SBARCH --n-task=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gpus=v100:1
 #SBATCH --mail-user=yxu783@uwo.ca
 #SBATCH --mail-type=FAIL
+
+module load python
 
 source $HOME/timesformer/bin/activate
 
