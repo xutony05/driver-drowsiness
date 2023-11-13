814b44f1f99aae11886d3c5d7bba41e703a6423b
Tony Xu <33339063+xutony05@users.noreply.github.com>
2023-11-12 23:26:34 -0500

    fix path

--- model/Transformer/HPC/TimeSformer/timesformer/__init__.py
+++ model/Transformer/HPC/TimeSformer/timesformer/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
-from utils.env import setup_environment
+from TimeSformer.timesformer.utils.env import setup_environment
 
 setup_environment()
